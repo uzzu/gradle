4181c2c9a02e736cc0fbf3213e6223213cc2a215
Daz DeBoer <daz@gradle.com>
2018-10-08 14:07:35 -0700

    force rerun 9

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/LargeDependencyGraphPerformanceTest.groovy
@@ -74,7 +74,7 @@ class LargeDependencyGraphPerformanceTest extends AbstractCrossVersionPerformanc
 
     def dummyMethod() {
         // Change this to force the performance tests to rerun
-        int forceRerun = 8
+        int forceRerun = 9
         return forceRerun
     }
 
